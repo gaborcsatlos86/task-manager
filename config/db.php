6019e58cedb8ea18cb4f574a92a71bb7621fc528
gaborcsatlos86 <gabor.csatlos86@gmail.com>
2016-09-13 22:05:47 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii_teszt',
     'username' => 'root',
-    'password' => 'W3bf3jl3szt0',
+    'password' => '',
     'charset' => 'utf8',
 ];
